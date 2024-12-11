1b88694b28ba5571951d30f75554ad7cfd1abf2d
zzarru <sn20180301@gmail.com>
2024-12-11 21:29:55 +0900

    BOJ 31403 B4 A+B-C

---
+++ BOJ/solved/class1/BOJ_31403_B4_A+B-C.py
@@ -0,0 +1,28 @@
+#0 입력받기 (정수)
+A = int(input())
+B = int(input())
+C = int(input())
+
+#1 정수로 생각하기
+print(A+B-C)
+
+#2 문자열로 생각하기
+strAB = str(A) + str(B)
+intAB = int(strAB)
+print(intAB-C)
+
+
+```
+1) input() -> 문자열로 입력을 받음
+2) print문 안에서 함수를 int(), str() 함수를 바로 썼다면 코드가 간결해졌을 거 같다.
+ex)
+A = input() #문자열
+B = input() #문자열
+C = int(input()) #정수
+
+#1 정수로 계산
+print(int(A)+int(B)-C)
+
+#2 문자열로 계산
+print(int(A+B)-C))
+```
